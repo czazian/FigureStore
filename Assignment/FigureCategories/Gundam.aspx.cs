c87f81bb22cdbce133cb6e19540140da7d9be07e
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-02-03 02:33:27 +0800

    Category Done for Gundam Only, All Others need to modify

--- Assignment/FigureCategories/Gundam.aspx.cs
+++ Assignment/FigureCategories/Gundam.aspx.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Diagnostics.Tracing;
 using System.EnterpriseServices;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -15,16 +16,10 @@ namespace Assignment.FigureCategories
     public partial class Gundam : System.Web.UI.Page
     {
 
-        string commandStatus = "";
-        string commandManufacturer = "";
-        string commandSeries = "";
-
-
         protected void Page_Init(object sender, EventArgs e)
         {
             //Initial View
             string status = Request.QueryString["status"];
-            string retrieve = "";
 
             SqlConnection conn;
             string str = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
@@ -32,23 +27,22 @@ namespace Assignment.FigureCategories
 
             conn.Open();
 
+            string retrieve = "";
+
             if (status == "all")
             {
                 RadioButtonList.SelectedIndex = 0;
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam' " ;
             }
             else if (status == "pre")
             {
                 RadioButtonList.SelectedIndex = 1;
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' AND FigureStatus = 'Pre-Order' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam' AND FigureStatus = 'Pre-Order'";
             }
             else if (status == "available")
             {
                 RadioButtonList.SelectedIndex = 2;
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' AND FigureStatus = 'Available' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam' AND FigureStatus = 'Available'";
             }
 
             SqlCommand cmd = new SqlCommand(retrieve, conn);
@@ -57,12 +51,10 @@ namespace Assignment.FigureCategories
 
             if (gundam.HasRows)
             {
-                while (gundam.Read())
-                {
-                    gundamRepater.DataSource = gundam;
-                    gundamRepater.DataBind();
-                }
-                resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+                gundamRepater.DataSource = gundam;
+                gundamRepater.DataBind();
+
+                resultno.Text = gundamRepater.Controls.Count + " Results";
             }
             else
             {
@@ -80,7 +72,7 @@ namespace Assignment.FigureCategories
             conn2 = new SqlConnection(str2);
 
             conn2.Open();
-            string allManufacturer = "SELECT DISTINCT FigureManufacturer FROM FIGURE WHERE FigureCategory = 'Gundam' ";
+            string allManufacturer = "SELECT DISTINCT [FigureManufacturer] FROM FIGURE WHERE [FigureCategory] = 'Gundam' ";
             SqlCommand cmd2 = new SqlCommand(allManufacturer, conn2);
             SqlDataReader rdr2 = cmd2.ExecuteReader();
 
@@ -97,7 +89,7 @@ namespace Assignment.FigureCategories
                 {
                     cbxManufacturer.Enabled = false;
                     onlyOneM.Visible = true;
-                    onlyOneM.Text = "There is only one manufacturer.";
+                    onlyOneM.Text = "There is only one manufacturer or no manufacturer.";
                 }
                 else
                 {
@@ -117,12 +109,12 @@ namespace Assignment.FigureCategories
             conn3 = new SqlConnection(str3);
 
             conn3.Open();
-            string allSeries = "SELECT DISTINCT FigureSeries FROM FIGURE WHERE FigureCategory = 'Gundam'";
+            string allSeries = "SELECT DISTINCT [FigureSeries] FROM FIGURE WHERE FigureCategory = 'Gundam'";
             SqlCommand cmd3 = new SqlCommand(allSeries, conn3);
             SqlDataReader rdr3 = cmd3.ExecuteReader();
 
 
-            List<String> sList = new List<String>();
+            List<string> sList = new List<string>();
 
             if (rdr3.HasRows)
             {
@@ -135,7 +127,7 @@ namespace Assignment.FigureCategories
                 {
                     cbxSeries.Enabled = false;
                     onlyOneS.Visible = true;
-                    onlyOneS.Text = "There is only one series.";
+                    onlyOneS.Text = "There is only one series or no series.";
                 }
                 else
                 {
@@ -154,6 +146,22 @@ namespace Assignment.FigureCategories
 
         protected void RadioButtonList_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+            //Clear Series and Manufacturer Filters 
+            if (cbxSeries.SelectedIndex != -1)
+            {
+                cbxSeries.SelectedIndex = -1;
+            }
+            if (cbxManufacturer.SelectedIndex != -1)
+            {
+                cbxManufacturer.SelectedIndex = -1;
+            }
+            minPrice.Text = "";
+            maxPrice.Text = "";
+            errorPrice.Visible = false;
+            ddlSort.SelectedIndex = -1;
+
+            //Main Operation
             gundamRepater.DataSource = null;
             gundamRepater.DataBind();
             SqlConnection conn2;
@@ -164,20 +172,18 @@ namespace Assignment.FigureCategories
 
             string retrieve = "";
 
+
             if (RadioButtonList.SelectedValue == "All Product")
             {
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam'";
             }
             else if (RadioButtonList.SelectedValue == "Pre-Order")
             {
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' AND FigureStatus = 'Pre-Order' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam' AND FigureStatus = 'Pre-Order' ";
             }
             else if (RadioButtonList.SelectedValue == "Available")
             {
-                commandStatus = "SELECT * FROM FIGURE WHERE FigureCategory = 'Gundam' AND FigureStatus = 'Available' ";
-                retrieve = commandStatus;
+                retrieve = "SELECT * FROM FIGURE WHERE [FigureCategory] = 'Gundam' AND FigureStatus = 'Available' ";
             }
 
             SqlCommand cmd = new SqlCommand(retrieve, conn2);
@@ -186,11 +192,9 @@ namespace Assignment.FigureCategories
 
             if (result.HasRows)
             {
-                while (result.Read())
-                {
-                    gundamRepater.DataSource = result;
-                    gundamRepater.DataBind();
-                }
+                gundamRepater.DataSource = result;
+                gundamRepater.DataBind();
+
                 resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
             }
             else
@@ -201,12 +205,276 @@ namespace Assignment.FigureCategories
 
         protected void cbxManufacturer_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //Deselect Status and Manufacturer
+            if (RadioButtonList.SelectedIndex != -1)
+            {
+                RadioButtonList.SelectedIndex = -1;
+            }
+            if (cbxSeries.SelectedIndex != -1)
+            {
+                cbxSeries.SelectedIndex = -1;
+            }
+            ddlSort.SelectedIndex = -1;
+            minPrice.Text = "";
+            maxPrice.Text = "";
+            errorPrice.Visible = false;
+
+            //If all filters is unselected
+            if (cbxManufacturer.SelectedIndex == -1 && RadioButtonList.SelectedIndex == -1 && cbxSeries.SelectedIndex == -1)
+            {
+                RadioButtonList.SelectedIndex = 0;
+                resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+            }
+
+            //Main Operation
+            SqlConnection conn5;
+            string str = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
+            conn5 = new SqlConnection(str);
+
+            //Get all selected items
+            List<ListItem> itemsSelected = cbxManufacturer.Items.Cast<ListItem>()
+                                                    .Where(li => li.Selected)
+                                                    .ToList();
+
+            //resultno.Text = itemsSelected.Count + " Results";
+            string commandManufacturer = "SELECT * FROM Figure WHERE [FigureCategory] = 'Gundam' AND FigureManufacturer IN(";
+            conn5.Open();
+
+            if (itemsSelected.Count != 0)
+            {
+                for (int i = 0; i < itemsSelected.Count; i++)
+                {
+                    commandManufacturer += "'" + itemsSelected[i].Text + "'";
+                    if ((i + 1) != itemsSelected.Count)
+                    {
+                        commandManufacturer += " , ";
+                    }
+                    else
+                    {
+                        commandManufacturer += "); ";
+                    }
+                }
+
+                SqlCommand cmd5 = new SqlCommand(commandManufacturer, conn5);
+
+                SqlDataReader mresult = cmd5.ExecuteReader();
+
+                if (mresult.HasRows)
+                {
+                    System.Diagnostics.Debug.Write("\n1 Round : " + commandManufacturer);
+
+                    gundamRepater.DataSource = mresult;
+                    gundamRepater.DataBind();
+
+                    resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+                }
+                else
+                {
+                    resultno.Text = "0" + " Results";
+                }
+            }
 
         }
+
+
 
         protected void cbxSeries_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+            //Deselect Status and Manufacturer
+            if (RadioButtonList.SelectedIndex != -1)
+            {
+                RadioButtonList.SelectedIndex = -1;
+            }
+            if (cbxManufacturer.SelectedIndex != -1)
+            {
+                cbxManufacturer.SelectedIndex = -1;
+            }
+            minPrice.Text = "";
+            maxPrice.Text = "";
+            errorPrice.Visible = false;
+            ddlSort.SelectedIndex = -1;
+
+            //If all filters is unselected
+            if (cbxManufacturer.SelectedIndex == -1 && RadioButtonList.SelectedIndex == -1 && cbxSeries.SelectedIndex == -1)
+            {
+                RadioButtonList.SelectedIndex = 0;
+                resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+            }
+
+            //Main Operation
+            SqlConnection conn4;
+            string str = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
+            conn4 = new SqlConnection(str);
+
+            //Get all selected items
+            List<ListItem> itemsSelected = cbxSeries.Items.Cast<ListItem>()
+                                                    .Where(li => li.Selected)
+                                                    .ToList();
+            //resultno.Text = itemsSelected.Count + " Results";
+            string commandSeries = "SELECT * FROM Figure WHERE [FigureCategory] = 'Gundam' AND FigureSeries IN(";
+            conn4.Open();
+
+            if (itemsSelected.Count != 0)
+            {
+                for (int i = 0; i < itemsSelected.Count; i++)
+                {
+                    commandSeries += "'" + itemsSelected[i].Text + "'";
+                    if ((i + 1) != itemsSelected.Count)
+                    {
+                        commandSeries += " , ";
+                    }
+                    else
+                    {
+                        commandSeries += "); ";
+                    }
+                }
+
+                SqlCommand cmd4 = new SqlCommand(commandSeries, conn4);
+
+                SqlDataReader sresult = cmd4.ExecuteReader();
+
+                if (sresult.HasRows)
+                {
+                    System.Diagnostics.Debug.Write("\n1 Round : " + commandSeries);
+
+                    gundamRepater.DataSource = sresult;
+                    gundamRepater.DataBind();
+
+                    resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+                }
+                else
+                {
+                    resultno.Text = "0" + " Results";
+                }
+            }
+        }
+
+        protected void clearAll_Click(object sender, EventArgs e)
+        {
+            //Clear Selection
+            cbxSeries.SelectedIndex = -1;
+            cbxManufacturer.SelectedIndex = -1;
+            minPrice.Text = "";
+            maxPrice.Text = "";
+            errorPrice.Visible = false;
+            ddlSort.SelectedIndex = -1;
+
+            //To Default
+            RadioButtonList.SelectedIndex = 0;
+            resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+        }
+
+        protected void Apply_Click(object sender, EventArgs e)
+        {
+            //Deselect Status and Manufacturer
+            if (RadioButtonList.SelectedIndex != -1)
+            {
+                RadioButtonList.SelectedIndex = -1;
+            }
+            if (cbxManufacturer.SelectedIndex != -1)
+            {
+                cbxManufacturer.SelectedIndex = -1;
+            }
+            if (cbxSeries.SelectedIndex != -1)
+            {
+                cbxSeries.SelectedIndex = -1;
+            }
+            ddlSort.SelectedIndex = -1;
+
+            int min = 0;
+            int max = 0;
+
+            //Main Operation
+            if (minPrice.Text == "" || maxPrice.Text == "")
+            {
+                errorPrice.Visible = true;
+                resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+            }
+            else
+            {
+                errorPrice.Visible = false;
+                min = Convert.ToInt32(minPrice.Text);
+                max = Convert.ToInt32(maxPrice.Text);
+
+                if (min >= max)
+                {
+                    errorPrice.Visible = true;
+                    resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+                }
+                else
+                {
+                    errorPrice.Visible = false;
+                    SqlConnection conn6;
+                    string str = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
+                    conn6 = new SqlConnection(str);
+
+                    conn6.Open();
+
+                    string cmdPrice = "SELECT * FROM Figure WHERE FigureCategory = 'Gundam' AND FigurePrice >= @min AND FigurePrice <= @max;";
+                    SqlCommand cmd6 = new SqlCommand(cmdPrice, conn6);
+                    cmd6.Parameters.AddWithValue("@min", min);
+                    cmd6.Parameters.AddWithValue("@max", max);
+                    SqlDataReader reader = cmd6.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        gundamRepater.DataSource = reader;
+                        gundamRepater.DataBind();
+                        resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+                    }
+                    else
+                    {
+                        gundamRepater.DataSource = "";
+                        gundamRepater.DataBind();
+                        resultno.Text = "0" + " Results";
+                    }
+                }
+            }
+        }
+
+        protected void ddlSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Reset
+            cbxSeries.SelectedIndex = -1;
+            cbxManufacturer.SelectedIndex = -1;
+            minPrice.Text = "";
+            maxPrice.Text = "";
+            errorPrice.Visible = false;
+            RadioButtonList.SelectedIndex = 0;
+            resultno.Text = gundamRepater.Controls.Count.ToString() + " Results";
+
+            //Main Operation
+            SqlConnection conn7;
+            string str = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
+            conn7 = new SqlConnection(str);
+
+            conn7.Open();
+
+            string cmdSort = "SELECT * FROM Figure WHERE FigureCategory = 'Gundam'";
+
+            if (ddlSort.SelectedValue == "1")
+            {
+                cmdSort += " ORDER BY FigurePrice ASC";
+            } else if (ddlSort.SelectedValue == "2")
+            {
+                cmdSort += " ORDER BY FigurePrice DESC";
+            } else if (ddlSort.SelectedValue == "3")
+            {
+                cmdSort += " ORDER BY FigureName ASC";
+            } else if (ddlSort.SelectedValue == "4")
+            {
+                cmdSort += " ORDER BY FigureName DESC";
+            }
+
+            SqlCommand cmd7 = new SqlCommand(cmdSort, conn7);
+            SqlDataReader r = cmd7.ExecuteReader();
+
+            if(r.HasRows)
+            {
+                gundamRepater.DataSource = r;
+                gundamRepater.DataBind();
+            } 
         }
     }
 }
--- Assignment/FigureCategories/Gundam.aspx.designer.cs
+++ Assignment/FigureCategories/Gundam.aspx.designer.cs
@@ -15,6 +15,33 @@ namespace Assignment.FigureCategories
     {
 
         /// <summary>
+        /// minPrice control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox minPrice;
+
+        /// <summary>
+        /// maxPrice control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox maxPrice;
+
+        /// <summary>
+        /// errorPrice control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label errorPrice;
+
+        /// <summary>
         /// RadioButtonList control.
         /// </summary>
         /// <remarks>
@@ -58,6 +85,15 @@ namespace Assignment.FigureCategories
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label onlyOneS;
+
+        /// <summary>
+        /// clearAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button clearAll;
 
         /// <summary>
         /// resultno control.
