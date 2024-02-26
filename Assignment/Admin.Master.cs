8a6d2e3e6a963c7e88bbddfcb3f12395f15eea80
Yiyang0519 <110144470+Yiyang0519@users.noreply.github.com>
2024-02-27 00:14:03 +0800

    27/2/2024(yy)

---
+++ Assignment/Admin.Master.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class Staff : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GetStaffNameByStaffID(string staffID)
+        {
+            string staffName = string.Empty;
+
+            string connectionString = ConfigurationManager.ConnectionStrings["NitroBooks"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "SELECT StaffName FROM Staff WHERE StaffID = @StaffID";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@StaffID", staffID);
+
+                    object staffNameObj = cmd.ExecuteScalar();
+
+                    if (staffNameObj != null)
+                    {
+                        staffName = staffNameObj.ToString();
+                    }
+                }
+            }
+
+            return staffName;
+        }
+
+        protected void Logout_Click(object sender, EventArgs e)
+        {
+            // Clear the session
+            Session["StaffID"] = null;
+
+            // Redirect to the homepage or the desired logout page
+            Response.Redirect("~/Customer/home.aspx");
+        }
+    }
+}
---
+++ Assignment/Staff/Dashboard.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeFile="Dashboard.aspx.cs" Inherits="Assignment.Staff.Dashboard" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
