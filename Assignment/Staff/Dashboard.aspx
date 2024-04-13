c81e4a5dfa04f27a47531ab4dc74f8aa72182bcf
Yiyang0519 <110144470+Yiyang0519@users.noreply.github.com>
2024-04-14 01:53:43 +0800

    14/4 yy

--- Assignment/Staff/Dashboard.aspx
+++ Assignment/Staff/Dashboard.aspx
@@ -4,6 +4,21 @@
         #dashbLi {
             background: #545454 ;
         }
+
+        .lbltxt {
+    font-size: 36px !important;
+    font-weight: 500 !important;
+    color: #ff7e29;
+}
+
+.box-topic {
+    font-weight: 550 !important;
+}
+
+.number {
+    font-size: 25px !important;
+    font-weight: 400 !important;
+}
     </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
@@ -11,33 +26,26 @@
 
         <div class="sales-boxes" style="padding-bottom:10px;">
             <div class="info3 box">
-                <div class="title">Welcome to Staff Portal</div>
-            </div>
+                <div class="title" style="color:#ff7e29; font-weight:600 !important;"><i class="bx bx-happy-alt"></i>&nbsp;Welcome to Staff Portal, XXX</div>
         </div>
 
-        <div class="overview-boxes">
-            <div class="box">
-                <div class="right-side">
-                    <div class="box-topic">Total Order</div>
+    <div class="overview-boxes">
+        <div class="box">
+            <div class="right-side">
+                <div class="box-topic">Total Order</div>
                 <div class="number">
-                    <asp:Label ID="lblTotalOrder" runat="server">0</asp:Label>
-                </div>
-                <div class="indicator">
-                    <!--                                <i class='bx bx-up-arrow-alt'></i>
-                                                            <span class="text">Up from yesterday</span>-->
+                    <asp:Label ID="lblTotalOrder" CssClass="lbltxt" runat="server">0</asp:Label>
+                     sales
                 </div>
             </div>
-            <i class='bx bx-cart-alt cart'></i>
+            <i class='bx bx-cart-alt cart' style="vertical-align:middle;"></i>
         </div>
         <div class="box">
             <div class="right-side">
                 <div class="box-topic">Quantity Sold</div>
                 <div class="number">
-                    <asp:Label ID="lblQuanSales" runat="server">0</asp:Label>
-                </div>
-                <div class="indicator">
-                    <!--                                <i class='bx bx-up-arrow-alt'></i>
-                                                            <span class="text">Up from yesterday</span>-->
+                    <asp:Label ID="lblQuanSales" CssClass="lbltxt" runat="server">0</asp:Label>
+                     units
                 </div>
             </div>
             <i class='bx bxs-cart-add cart two'></i>
@@ -47,11 +55,7 @@
                 <div class="box-topic">Sales Profit</div>
                 <div class="number">
                     RM 
-                    <asp:Label ID="lblSalesProfit" runat="server">0</asp:Label>
-                </div>
-                <div class="indicator">
-                    <!--                                <i class='bx bx-up-arrow-alt'></i>
-                                                            <span class="text">Up from yesterday</span>-->
+        <asp:Label ID="lblSalesProfit" CssClass="lbltxt" runat="server">0</asp:Label>
                 </div>
             </div>
             <i class='bx bxs-cart-add cart three'></i>
@@ -61,9 +65,7 @@
                 <div class="box-topic">Total Profit</div>
                 <div class="number">
                     RM
-                    <asp:Label ID="lblTotalProfit" runat="server">0</asp:Label>
-                </div>
-                <div class="indicator">
+        <asp:Label ID="lblTotalProfit" CssClass="lbltxt" runat="server">0</asp:Label>
                 </div>
             </div>
             <i class='bx bx-cart cart three'></i>
@@ -85,13 +87,77 @@
 <hr />
             </div>
         </div>
-
-
 </div>
+
+        <asp:HiddenField ID="lblTotalJanOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalFebOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalMacOrder"  runat="server" />
+        <asp:HiddenField ID="lblTotalAprOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalMayOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalJuneOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalJulyOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalOgosOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalSepOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalOctOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalNovOrder" runat="server" />
+        <asp:HiddenField ID="lblTotalDecOrder" runat="server" />
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <script src="DashboardChartA.js"></script>
-    <script src="DashboardChartB.js"></script>
+    <script>
+        const ctx = document.getElementById('line');
+        const janTotal = document.getElementById('<%= lblTotalJanOrder.ClientID %>').value;
+        const febTotal = document.getElementById('<%= lblTotalFebOrder.ClientID %>').value;
+        const macTotal = document.getElementById('<%= lblTotalMacOrder.ClientID %>').value;
+        const aprTotal = document.getElementById('<%= lblTotalAprOrder.ClientID %>').value;
+        const mayTotal = document.getElementById('<%= lblTotalMayOrder.ClientID %>').value;
+        const juneTotal = document.getElementById('<%= lblTotalJuneOrder.ClientID %>').value;
+        const julyTotal = document.getElementById('<%= lblTotalJulyOrder.ClientID %>').value;
+        const ogosTotal = document.getElementById('<%= lblTotalOgosOrder.ClientID %>').value;
+        const sepTotal = document.getElementById('<%= lblTotalSepOrder.ClientID %>').value;
+        const octTotal = document.getElementById('<%= lblTotalOctOrder.ClientID %>').value;
+        const novTotal = document.getElementById('<%= lblTotalNovOrder.ClientID %>').value;
+        const decTotal = document.getElementById('<%= lblTotalDecOrder.ClientID %>').value;
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Jan', 'Feb', 'Mac', 'Apr', 'May', 'June', 'July', 'Ogos', 'Sept', 'Oct', 'Nov', 'Dec'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [janTotal, febTotal, macTotal, aprTotal, mayTotal, juneTotal, julyTotal, ogosTotal, sepTotal, octTotal, novTotal, decTotal],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const ctx2 = document.getElementById('pie');
+
+        new Chart(ctx2, {
+            type: 'pie',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 
 </asp:Content>
--- Assignment/Staff/Dashboard.aspx.cs
+++ Assignment/Staff/Dashboard.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,7 +14,128 @@ namespace Assignment.Staff
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            SqlConnection cnn1, cnn2, cnn3, cnn4, cnn5, cnn6, cnn7, cnn8, cnn9, cnn10, cnn11, cnn12, cnn13, cnn14, cnn15, cnn16;
+            string strConnection = ConfigurationManager.ConnectionStrings["ApexOnlineShopDb"].ConnectionString;
+            cnn1 = new SqlConnection(strConnection);
+            cnn2 = new SqlConnection(strConnection);
+            cnn3 = new SqlConnection(strConnection);
+            cnn4 = new SqlConnection(strConnection);
+            cnn5 = new SqlConnection(strConnection);
+            cnn6 = new SqlConnection(strConnection);
+            cnn7 = new SqlConnection(strConnection);
+            cnn8 = new SqlConnection(strConnection);
+            cnn9 = new SqlConnection(strConnection);
+            cnn10 = new SqlConnection(strConnection);
+            cnn11 = new SqlConnection(strConnection);
+            cnn12 = new SqlConnection(strConnection);
+            cnn13 = new SqlConnection(strConnection);
+            cnn14 = new SqlConnection(strConnection);
+            cnn15 = new SqlConnection(strConnection);
+            cnn16 = new SqlConnection(strConnection);
+
+            cnn1.Open();
+            cnn2.Open();
+            cnn3.Open();
+            cnn4.Open();
+            cnn5.Open();
+            cnn6.Open();
+            cnn7.Open();
+            cnn8.Open();
+            cnn9.Open();
+            cnn10.Open();
+            cnn11.Open();
+            cnn12.Open();
+            cnn13.Open();
+            cnn14.Open();
+            cnn15.Open();
+            cnn16.Open();
+
+            String sql1 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID";
+            String sql2 = "SELECT SUM(OrderQuantity) FROM [OrderFigure], [Order] WHERE [OrderFigure].OrderID = [Order].OrderID";
+            String sql3 = "SELECT SUM([OrderFigure].OrderQuantity*[Figure].FigurePrice) AS 'Total Price' FROM [OrderFigure], [Order], [Figure] WHERE [Figure].FigureID = [OrderFigure].FigureID AND [OrderFigure].OrderID = [Order].OrderID";
+            String sql4 = "SELECT SUM(PaymentAmount) FROM [OrderFigure], [Order] WHERE [OrderFigure].OrderID = [Order].OrderID";
+            String sql5 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 1 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql6 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 2 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql7 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 3 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql8 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 4 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql9 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 5 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql10 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 6 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql11 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 7 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql12 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 8 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql13 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 9 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql14 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 10 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql15 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 11 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+            String sql16 = "SELECT COUNT(*) FROM [Order], [OrderFigure] WHERE [Order].OrderID = [OrderFigure].OrderID AND MONTH([Order].OrderDate) = 12 AND YEAR([Order].OrderDate) = YEAR(GETDATE())";
+
+
+            SqlCommand cmdRetrieve1 = new SqlCommand(sql1, cnn1);
+            SqlCommand cmdRetrieve2 = new SqlCommand(sql2, cnn2);
+            SqlCommand cmdRetrieve3 = new SqlCommand(sql3, cnn3);
+            SqlCommand cmdRetrieve4 = new SqlCommand(sql4, cnn4);
+            SqlCommand cmdRetrieve5 = new SqlCommand(sql5, cnn5);
+            SqlCommand cmdRetrieve6 = new SqlCommand(sql6, cnn6);
+            SqlCommand cmdRetrieve7 = new SqlCommand(sql7, cnn7);
+            SqlCommand cmdRetrieve8 = new SqlCommand(sql8, cnn8);
+            SqlCommand cmdRetrieve9 = new SqlCommand(sql9, cnn9);
+            SqlCommand cmdRetrieve10 = new SqlCommand(sql10, cnn10);
+            SqlCommand cmdRetrieve11 = new SqlCommand(sql11, cnn11);
+            SqlCommand cmdRetrieve12 = new SqlCommand(sql12, cnn12);
+            SqlCommand cmdRetrieve13 = new SqlCommand(sql13, cnn13);
+            SqlCommand cmdRetrieve14 = new SqlCommand(sql14, cnn14);
+            SqlCommand cmdRetrieve15 = new SqlCommand(sql15, cnn15);
+            SqlCommand cmdRetrieve16 = new SqlCommand(sql16, cnn16);
+
+            SqlDataReader totalOrder = cmdRetrieve1.ExecuteReader();
+            SqlDataReader quanSales = cmdRetrieve2.ExecuteReader();
+            SqlDataReader salesProfit = cmdRetrieve3.ExecuteReader();
+            SqlDataReader totalProfit = cmdRetrieve4.ExecuteReader();
+            SqlDataReader totalJanOrder = cmdRetrieve5.ExecuteReader();
+            SqlDataReader totalFebOrder = cmdRetrieve6.ExecuteReader();
+            SqlDataReader totalMacOrder = cmdRetrieve7.ExecuteReader();
+            SqlDataReader totalAprOrder = cmdRetrieve8.ExecuteReader();
+            SqlDataReader totalMayOrder = cmdRetrieve9.ExecuteReader();
+            SqlDataReader totalJuneOrder = cmdRetrieve10.ExecuteReader();
+            SqlDataReader totalJulyOrder = cmdRetrieve11.ExecuteReader();
+            SqlDataReader totalOgosOrder = cmdRetrieve12.ExecuteReader();
+            SqlDataReader totalSepOrder = cmdRetrieve13.ExecuteReader();
+            SqlDataReader totalOctOrder = cmdRetrieve14.ExecuteReader();
+            SqlDataReader totalNovOrder = cmdRetrieve15.ExecuteReader();
+            SqlDataReader totalDecOrder = cmdRetrieve16.ExecuteReader();
+
+            if ((totalOrder.HasRows && totalOrder.Read()) && (salesProfit.HasRows && salesProfit.Read()) && (quanSales.HasRows && quanSales.Read()) && (totalProfit.HasRows && totalProfit.Read()) && (totalJanOrder.HasRows && totalJanOrder.Read()) && (totalFebOrder.HasRows && totalFebOrder.Read()) && (totalMacOrder.HasRows && totalMacOrder.Read()) && (totalAprOrder.HasRows && totalAprOrder.Read()) && (totalMayOrder.HasRows && totalMayOrder.Read()) && (totalJuneOrder.HasRows && totalJuneOrder.Read()) && (totalJulyOrder.HasRows && totalJulyOrder.Read()) && (totalOgosOrder.HasRows && totalOgosOrder.Read()) && (totalSepOrder.HasRows && totalSepOrder.Read()) && (totalOctOrder.HasRows && totalOctOrder.Read()) && (totalNovOrder.HasRows && totalNovOrder.Read()) && (totalDecOrder.HasRows && totalDecOrder.Read()))
+            {
+                lblTotalOrder.Text = totalOrder.GetValue(0).ToString();
+                lblQuanSales.Text = quanSales.GetValue(0).ToString();
+                lblSalesProfit.Text = salesProfit.GetValue(0).ToString();
+                lblTotalProfit.Text = totalProfit.GetValue(0).ToString();
+
+                int lblTotalJanOrder = Convert.ToInt32(totalJanOrder.GetValue(0));
+                int lblTotalFebOrder = Convert.ToInt32(totalFebOrder.GetValue(0));
+                int lblTotalMacOrder = Convert.ToInt32(totalMacOrder.GetValue(0));
+                int lblTotalAprOrder = Convert.ToInt32(totalAprOrder.GetValue(0));
+                Debug.Write("testing: " + lblTotalAprOrder);
+                int lblTotalMayOrder = Convert.ToInt32(totalMayOrder.GetValue(0));
+                int lblTotalJuneOrder = Convert.ToInt32(totalJuneOrder.GetValue(0));
+                int lblTotalJulyOrder = Convert.ToInt32(totalJulyOrder.GetValue(0));
+                int lblTotalOgosOrder = Convert.ToInt32(totalOgosOrder.GetValue(0));
+                int lblTotalSepOrder = Convert.ToInt32(totalSepOrder.GetValue(0));
+                int lblTotalOctOrder = Convert.ToInt32(totalOctOrder.GetValue(0));
+                int lblTotalNovOrder = Convert.ToInt32(totalNovOrder.GetValue(0));
+                int lblTotalDecOrder = Convert.ToInt32(totalDecOrder.GetValue(0));
+
+                //if total order = 0 means no sales then put 0 for all label
+                if (totalOrder.GetValue(0).ToString() == "0")
+                {
+                    lblQuanSales.Text = "0";
+                    lblSalesProfit.Text = "0";
+                    lblTotalProfit.Text = "0";
+                }
+            }
+            else
+            {
+                Response.Write("Invalid Records");
+            }
+
         }
 
         
